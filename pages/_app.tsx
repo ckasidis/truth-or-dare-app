3b91d6b250b7fdf293e0ee437c7be5c457b7978e
Kasidis Chantharojwong <me@ckasidis.com>
2022-08-30 21:35:12 +0800

    add Dark Mode support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
+import theme from '../theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
 	const queryClient = new QueryClient();
 	return (
 		<QueryClientProvider client={queryClient}>
-			<ChakraProvider>
+			<ChakraProvider theme={theme}>
 				<Component {...pageProps} />
 			</ChakraProvider>
 		</QueryClientProvider>
