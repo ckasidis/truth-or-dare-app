c5459bf80c30fc79d62a3cc0acd346a86620ab48
Kasidis Chantharojwong <me@ckasidis.com>
2022-08-31 02:52:29 +0800

    add curPlayer and randomPlayer in zustand store

--- lib/store.ts
+++ lib/store.ts
@@ -3,15 +3,18 @@ import create from 'zustand';
 interface GameSettingsState {
 	mode: 'truth' | 'dare' | 'truthOrDare';
 	players: string[];
+	curPlayer: string | null;
 	setMode: (mode: 'truth' | 'dare' | 'truthOrDare') => void;
 	addPlayer: (player: string) => void;
 	removePlayer: (player: string) => void;
 	clearPlayers: () => void;
+	randomPlayer: () => void;
 }
 
 export const useStore = create<GameSettingsState>((set) => ({
 	mode: 'truthOrDare',
 	players: [],
+	curPlayer: null,
 	setMode: (mode) => set(() => ({ mode })),
 	addPlayer: (player) =>
 		set((state) => ({
@@ -22,4 +25,10 @@ export const useStore = create<GameSettingsState>((set) => ({
 			players: state.players.filter((cur) => cur !== player),
 		})),
 	clearPlayers: () => set(() => ({ players: [] })),
+	randomPlayer: () =>
+		set((state) => ({
+			curPlayer: state.players.length
+				? state.players[Math.floor(Math.random() * state.players.length)]
+				: null,
+		})),
 }));
